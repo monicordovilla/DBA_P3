a0a04f9c161839fe708aeb515b57b2babde75aea
AnaBosch <39536972+AnaBosch@users.noreply.github.com>
2019-12-01 22:03:23 +0100

    Planteamiento incial búsqueda (pseudocódigo)

--- DBAPrac3/src/dbaprac3/AgenteFly.java
+++ DBAPrac3/src/dbaprac3/AgenteFly.java
@@ -20,4 +20,45 @@ public class AgenteFly extends AgenteDron {
         tamanio_radar = 5;
         centro_radar = 2;
     }
+    
+    /**
+    *
+    * @author Ana
+    * Selección de la estrategia de búsqueda que va a seguir en función del mapa y grupo de rescate que se elija
+    */
+    public void selectorEstrategia(){//Variable que tenemos que ver donde recoje su valor)
+        if(variable)
+            //Realizas esta eleccion
+        else
+            //esta   
+    }
+    
+    /**
+    *
+    * @author Ana
+    * Estrategia de búsqueda para mapas altos en los que el equipo esta formado por 2 Fly y 2 Rescue
+    */
+    public void estrategiaMapasAltos(){//Variable que tenemos que ver donde recoje su valor)
+        //Cuando creemos las instancias debemos establecerle un identificador para que aqui segn eso recorra una parte del mapa
+        if(encuentra)
+            //busqueda directa
+    }
+    
+    /**
+    *
+    * @author Ana
+    * Estrategia de búsqueda para mapas bajos en los que el equipo esta formado por 1 Fly, 1 Hawk y 2 Rescue
+    */
+    public void estrategiaMapasBajos(){//Variable que tenemos que ver donde recoje su valor)
+        //if encuentra busqueda directa
+    }
+    
+    /**
+    *
+    * @author Ana
+    * Se conoce donde esta el objetivo y se dirige directamente hacia el
+    */
+    public void estrategiaMapasBajos(){//Variable que tenemos que ver donde recoje su valor)
+        //if encuentra busqueda directa
+    }
 }
